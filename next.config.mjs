9b3bf07c06b64f1986d9b11fd6f6fd027ee4d75e
Sathwik Doddi <sathwik.doddi@gmail.com>
2025-06-05 02:24:43 -0500

    vercel optimized

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    optimizeCss: true,
+  },
+};
+
+export default nextConfig; 
